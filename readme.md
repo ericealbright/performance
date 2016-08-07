296aceb161479e14a4361b38def41e9df94c5358
ericealbright <eric.e.albright@gmail.com>
2016-08-06 18:02:45 -0700

    Next Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+Performance
+========
+
+Intro
+--------
+Performance
---
+++ src/main.js
@@ -0,0 +1,16 @@
+(function() {
+	"use strict";
+
+	function getCopyString() {
+		return 0;
+	}
+
+	function getLocation() {
+
+	}
+
+	function getDistance() {
+		
+	}
+
+}());
